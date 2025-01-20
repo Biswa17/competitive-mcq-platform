3f4df53e15196a9c45ee29568b75630426406b4a
Biswa17 <bswbrh3@gmail.com>
2025-01-20 16:15:54 +0530

    Initial commit with utility helper added

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function response($data,$status,$message='',$other=array())
+    {
+    	$response = array();
+        //$data = checkEnv($data);
+        //$data['cdn_path'] = \Config::get('mgl.cdn_path');
+        // p($data);
+        if($status==200)
+    	{
+    		$response = array('status'=>'success','status_code'=>$status,'message'=>$message,'response'=>$data);
+		}
+    	elseif($status=='')
+    	{
+            $status = 203;
+    		$response = array('status'=>'failed','status_code'=>$status,'message'=>($message!=''?$message:'Invalid request!'),'response'=>array('errors'=>'something went wrong or validation issue.'));
+    	}
+		else
+    	{
+    		$response = array('status'=>'failed','status_code'=>$status,'message'=>$message,'response'=>array('errors'=>$data));
+    	}
+        
+
+        if(isset($other['cache']['key']) && $status==200)
+        {
+            if($other['cache']['ttl'] > 0)
+            $response = storeOrUpdateCache($other['cache']['key'],$response,$other['cache']['ttl']);
+        }
+
+    	return response()->json($response,$status);
+
+    }
+}
